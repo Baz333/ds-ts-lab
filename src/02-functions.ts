66ff31d883eb21e6e6b7c281a00985ee3c19889c
Barry Nolan <bazdagreat333@gmail.com>
2024-09-16 20:15:18 +0100

    Typed function signature

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,1 +1,43 @@
 import { Friend, Colleague } from "./myTypes";
+import { colleagues, friends } from "./01-basics";
+
+function older(f: Friend) : string {
+    f.age += 1
+    return `${f.name} is now ${f.age}`
+}
+
+// console.log(older(friends[0]))
+
+function allOlder(friends: Friend[]) : string[] {
+    const array: string[] = [];
+    friends.forEach(friend => {
+        array.push(older(friend));
+    });
+    return array;
+}
+
+// console.log(allOlder(friends))
+
+function highestExtension(cs: Colleague[]) : Colleague {
+    const result = cs.sort(
+        (c1, c2) => c1.contact.extension - c2.contact.extension
+    );
+    return result[cs.length - 1];
+}
+
+// console.log(highestExtension(colleagues.current));
+
+function addColleague(cs: Colleague[], name: string, dept: string, email: string) : void {
+    const newColleague = {
+        name: name,
+        department: dept,
+        contact: {
+            email: email,
+            extension: highestExtension(cs).contact.extension + 1,
+        },
+    }
+    cs.push(newColleague);
+}
+
+addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
